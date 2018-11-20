70baaa75081854bf0889caec2ff6d63ec920bd63
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-11-20 11:59:55 -0500

    Updates

---
+++ precache-manifest.3050fe0240ffe5a849d25241c73c102e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb5d57de38284f0a3d03",
+    "url": "/todos-part-3/static/js/runtime~main.eb5d57de.js"
+  },
+  {
+    "revision": "3c1c6066a1cea1a462ff",
+    "url": "/todos-part-3/static/js/main.3c1c6066.chunk.js"
+  },
+  {
+    "revision": "224f1180f18ca317baad",
+    "url": "/todos-part-3/static/js/1.224f1180.chunk.js"
+  },
+  {
+    "revision": "3c1c6066a1cea1a462ff",
+    "url": "/todos-part-3/static/css/main.cab62a45.chunk.css"
+  },
+  {
+    "revision": "faf94a41b26656536ff28447f4a07583",
+    "url": "/todos-part-3/index.html"
+  }
+];
