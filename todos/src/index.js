7e11330314a07ea63f63fbeb5d50e4e82d7b6d6f
Jenny Hoelzer <hoelzer.jenny@gmail.com>
2018-11-20 12:02:54 -0500

    attempt 1

--- todos/src/index.js
+++ todos/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 
 const Index = () => (
-    <BrowserRouter basename={ProcessingInstruction.env.PUBLIC_URL}>
+    <BrowserRouter>
         <App/>
     </BrowserRouter>
 );
